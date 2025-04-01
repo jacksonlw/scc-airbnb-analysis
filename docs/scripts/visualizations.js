500fc41df3e13a7b84ff19147669ab339641e62b
Jackson Lawrence <jlawrence1787@gmail.com>
2025-04-01 19:31:55 -0400

    added scatter plot

--- docs/scripts/visualizations.js
+++ docs/scripts/visualizations.js
@@ -1,1 +1,97 @@
 // Visualization script (with D3JS)
+
+let width = 900,
+  height = 500;
+
+let margin = {
+  top: 50,
+  bottom: 50,
+  left: 60,
+  right: 50,
+};
+
+const scatterPlot = (data) => {
+  let svg = d3
+    .select("#plot-price-score-scatter")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .style("background", "#f1f4f8");
+
+  //Define the scale
+  let yscale = d3
+    .scaleLinear()
+    .domain([0, 365])
+    .range([height - margin.bottom, margin.top]);
+
+  let xscale = d3
+    .scaleLinear()
+    .domain([0, 2000])
+    .range([margin.left, width - margin.right]);
+
+  //Draw the scale
+  let yaxis = svg
+    .append("g")
+    .call(d3.axisLeft().scale(yscale))
+    .attr("transform", `translate(${margin.left} , 0)`);
+
+  let xaxis = svg
+    .append("g")
+    .call(d3.axisBottom().scale(xscale))
+    .attr("transform", `translate(0,${height - margin.bottom})`);
+
+  const color = d3.scaleSequential(d3.interpolateViridis);
+
+  //Draw the circles
+  let circles = svg
+    .selectAll("circle")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("r", 4)
+    .attr("cx", (d) => xscale(d.price))
+    .attr("cy", (d) => yscale(d.availability_365))
+    .attr("fill", (d) => color(d.performance_score / 100))
+    .attr("opacity", 0.7)
+    .attr("stroke", "#000");
+
+  //Draw the labels
+  svg
+    .append("text")
+    .text("Price per night (USD)")
+    .attr("x", width / 2)
+    .attr("y", height - 15)
+    .attr("text-anchor", "middle");
+
+  svg
+    .append("text")
+    .text("Availability over next year")
+    .attr("x", 0 - height / 2)
+    .attr("y", 25)
+    .attr("transform", "rotate(-90)")
+    .attr("text-anchor", "middle");
+};
+
+async function loadData() {
+  const data = await d3.csv("../scc_airbnbs.csv");
+
+  // Convert the performance score and price to numbers
+  data.forEach((d) => {
+    d.performance_score = +d.performance_score;
+    d.price = +d.price;
+    d.accommodates = +d.accommodates;
+    d.bathrooms = +d.bathrooms;
+    d.bedrooms = +d.bedrooms;
+    d.beds = +d.beds;
+    d.availability_365 = +d.availability_365;
+    d.amenities = JSON.parse(d.amenities);
+  });
+
+  console.log(data);
+
+  return data;
+}
+
+loadData().then((data) => {
+  scatterPlot(data);
+});
