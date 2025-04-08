634d13b776f6182ed5a7079e237340ee1bc0eaa8
Jackson Lawrence <jlawrence1787@gmail.com>
2025-04-08 17:32:01 -0400

    added price vs room type chart

--- docs/scripts/altair.js
+++ docs/scripts/altair.js
@@ -3,3 +3,9 @@ vegaEmbed("#plot-amenity-boxplot", "charts/amenities_chart.json")
     // Access the Vega view instance as result.view
   })
   .catch(console.error);
+
+vegaEmbed("#plot-room-type-price", "charts/room_type_vs_price_chart.json")
+  .then(function (result) {
+    // Access the Vega view instance as result.view
+  })
+  .catch(console.error);
